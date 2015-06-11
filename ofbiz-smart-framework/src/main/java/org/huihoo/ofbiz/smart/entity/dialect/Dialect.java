844a56611def88751598ca1b0a75a0b6261e3eb7
Administrator <Administrator@PC-201502261028>
2015-06-11 16:54:22 +0800

    数据库方言接口提交

---
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/dialect/Dialect.java
@@ -0,0 +1,14 @@
+package org.huihoo.ofbiz.smart.entity.dialect;
+
+/**
+ * <p>
+ * 数据库方言接口，它定义了 各种数据库要实现的接口。
+ * </p>
+ * 
+ * @author huangbaihua
+ * @version 1.0
+ * @since 1.0
+ */
+public interface Dialect {
+  
+}
--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/dialect/package-info.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/dialect/package-info.java
@@ -1,5 +1,8 @@
 /**
+ * <p>
+ *  Thd database 
+ * </p>
  * @author huangbaihua
- *
+ * 
  */
 package org.huihoo.ofbiz.smart.entity.dialect;
