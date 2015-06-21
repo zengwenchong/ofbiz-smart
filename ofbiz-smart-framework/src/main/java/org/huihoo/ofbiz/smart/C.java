06472c4b13d03df28e539417c872ff012aa80fd0
huangbaihua <huangbaihua001@163.com>
2015-06-21 18:08:44 +0800

    增加常量接口

---
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/C.java
@@ -0,0 +1,18 @@
+package org.huihoo.ofbiz.smart;
+
+/**
+ * <p>
+ * 常量接口，它包含有框架定义的全局常量。
+ * </p>
+ * 
+ * @author huangbaihua
+ * @version 1.0
+ * @since 1.0
+ */
+public interface C {
+
+  /** 系统换行符 */
+  public final static String LINE_SEPARATOR = System.getProperty("line.separator");
+  /** 文件分隔符 */
+  public final static String FILE_SEPARATOR = System.getProperty("file.separator");
+}
