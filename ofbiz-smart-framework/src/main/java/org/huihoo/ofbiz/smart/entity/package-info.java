6d65784f32af571589acd0e71057da77ad5b8441
huangbaihua <huangbaihua001@163.com>
2015-06-22 19:25:13 +0800

    Update Package Comments.

--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/package-info.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/package-info.java
@@ -1,6 +1,13 @@
 /**
  * <p>
- * Activerecord Parttern Implemention
+ *  参考<a href='https://de.wikipedia.org/wiki/Active_Record'>ActiveRecord</a>模式.<br/>
+ *  结合实际项目的开发经验，一个非标准，简易的，总体思路一致的实现。
+ * </p>
+ * <p>
+ *  参考:<br/>
+ *  1. 设计思想参考了<a href='http://guides.rubyonrails.org'>Ruby On Rails Guides</a><br/>
+ *  2. 具体实现部分参考了<a href='https://github.com/javalite/activejdbc'>javalite activejdbc</a>。<br/>
+ *     在它的基础上，结合实际情况进行了简化，重构。总体上控制代码的规模，毕竟它还是很庞大的。<br/>
  * </p>
  * 
  * @author huangbaihua
