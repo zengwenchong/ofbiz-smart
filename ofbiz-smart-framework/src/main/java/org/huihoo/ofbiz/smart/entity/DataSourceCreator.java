9040ae4a8c165f5bb9f14ee01a278cc6eed9e830
huangbaihua <huangbaihua001@163.com>
2015-06-26 19:08:43 +0800

    Add Comments

--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/DataSourceCreator.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/DataSourceCreator.java
@@ -6,6 +6,23 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+/**
+ * <p>
+ * 数据源创建接口
+ * </p>
+ * 
+ * @author huangbaihua
+ * @version 1.0
+ * @since 1.0
+ * @param <K>
+ * @param <V>
+ */
 public interface DataSourceCreator<K, V extends DataSource> {
+  /**
+   * 创建数据源
+   * 
+   * @param config 数据源属性配置
+   * @return 创建成功之后的数据源<code>Map</code>,其中键为数据源的名称，值为<code>javax.sql.DataSource</code>接口实例
+   */
   public Map<K, V> createDataSources(Properties config);
 }
--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/DefaultDataSourceCreator.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/DefaultDataSourceCreator.java
@@ -10,7 +10,17 @@ import java.util.Set;
 import javax.sql.DataSource;
 
 import org.huihoo.ofbiz.smart.C;
-
+/**
+ * <p>
+ *  默认数据源创建{@link DataSourceCreator}接口实现
+ * </p>
+ * <p>
+ *  根据数据源属性配置，生成多个<code>{@link SimpleDataSource}</code>数据源实例
+ * </p>
+ * @author  huangbaihua
+ * @version 1.0
+ * @since   1.0
+ */
 public class DefaultDataSourceCreator implements DataSourceCreator<String, DataSource> {
 
   @Override
@@ -30,21 +40,24 @@ public class DefaultDataSourceCreator implements DataSourceCreator<String, DataS
     
     for (String dName : dataSourceNames) {
       String provider = config.getProperty("datasource."+dName+".provider");
-      String driver = config.getProperty("datasource."+dName+".driver");
-      String username = config.getProperty("datasource."+dName+".username");
-      String password = config.getProperty("datasource."+dName+".password");
-      String url = config.getProperty("datasource."+dName+".url");
       if(C.DEF_DATASOURCE_PROVIDER.equals(provider)){
+        String driver = config.getProperty("datasource."+dName+".driver");
+        String username = config.getProperty("datasource."+dName+".username");
+        String password = config.getProperty("datasource."+dName+".password");
+        String url = config.getProperty("datasource."+dName+".url");
+        int maxConnections = Integer.valueOf(config.getProperty("datasource."+dName+".maxConnections", "16"));
+        int minConnections = Integer.valueOf(config.getProperty("datasource."+dName+".minConnections", "16"));
+        
         SimpleDataSource ds = new SimpleDataSource();
         ds.setDriverClassName(driver);
         ds.setUserName(username);
         ds.setPassword(password);
         ds.setUrl(url);
+        ds.setMaxConnections(maxConnections);
+        ds.setMaxConnections(minConnections);
         dataSources.put(dName, ds);
       }
     }
-    
     return dataSources;
   }
-
 }
