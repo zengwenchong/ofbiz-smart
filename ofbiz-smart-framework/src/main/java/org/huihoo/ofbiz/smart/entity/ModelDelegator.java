d15fee6c8ff80edc4ae9c140c89a0b8412a5b934
huangbaihua <huangbaihua001@163.com>
2015-06-28 00:05:11 +0800

    Update ModelDelegator.

--- ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/ModelDelegator.java
+++ ofbiz-smart-framework/src/main/java/org/huihoo/ofbiz/smart/entity/ModelDelegator.java
@@ -1,5 +1,8 @@
 package org.huihoo.ofbiz.smart.entity;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import org.huihoo.ofbiz.smart.base.util.Log;
 
 public class ModelDelegator {
@@ -7,14 +10,28 @@ public class ModelDelegator {
 
   public static <T extends Model> T create(Class<T> clazz, Object... namesAndValues) {
     try {
-      T model = clazz.newInstance().set(namesAndValues);
+      Constructor<T> c = clazz.getDeclaredConstructor();
+      c.setAccessible(true);
+      T model = c.newInstance(clazz).set(namesAndValues);
       model.save();
       return model;
     } catch (InstantiationException e) {
-      Log.e(e, tag, "ModelDelegator.create() exception :" + e.getMessage());
+      Log.e(e, tag, e.getMessage());
       throw new EntityException(e);
     } catch (IllegalAccessException e) {
-      Log.e(e, tag, "ModelDelegator.create() exception :" + e.getMessage());
+      Log.e(e, tag, e.getMessage());
+      throw new EntityException(e);
+    } catch (IllegalArgumentException e) {
+      Log.e(e, tag, e.getMessage());
+      throw new EntityException(e);
+    } catch (InvocationTargetException e) {
+      Log.e(e, tag, e.getMessage());
+      throw new EntityException(e);
+    } catch (NoSuchMethodException e) {
+      Log.e(e, tag, e.getMessage());
+      throw new EntityException(e);
+    } catch (SecurityException e) {
+      Log.e(e, tag, e.getMessage());
       throw new EntityException(e);
     }
   }
