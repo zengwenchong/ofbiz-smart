738df4509d39e136715078684543c7ff3511629c
huangbaihua <huangbaihua001@gmail.com>
2015-07-01 15:32:20 +0800

    转换加强Model实现类的modelClass方法

--- ofbiz-smart-agent/src/main/java/org/huihoo/ofbiz/smart/agent/Transformer.java
+++ ofbiz-smart-agent/src/main/java/org/huihoo/ofbiz/smart/agent/Transformer.java
@@ -11,12 +11,13 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 
-public class Transformer implements ClassFileTransformer {
+public class Transformer implements ClassFileTransformer,Opcodes {
   private static final Logger log = LoggerFactory.getLogger(Transformer.class);
   private IgnoreClassHelper ignoreClassHelper;
 
@@ -57,6 +58,18 @@ public class Transformer implements ClassFileTransformer {
       ClassVisitor cv = new EnhanceModelAdapter(Opcodes.ASM5, cw);
       ClassReader cr = new ClassReader(classfileBuffer);
       cr.accept(cv, 0);
+      
+      String parentCName = "org/huihoo/ofbiz/smart/entity/Model";
+      String convertCName = className.replaceAll("\\.", "/");
+      String cType = "L"+convertCName+";";
+      MethodVisitor mv = null;
+      mv = cw.visitMethod(ACC_PROTECTED, "modelClass", "()Ljava/lang/Class;", "<T:L"+parentCName+";>()Ljava/lang/Class<TT;>;", null);
+      mv.visitCode();
+      mv.visitLdcInsn(Type.getType(cType));
+      mv.visitInsn(ARETURN);
+      mv.visitMaxs(1, 1);
+      mv.visitEnd();
+      
       return cw.toByteArray();
     }catch(Exception e){
       log.error(e.getMessage(), e);
