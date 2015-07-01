76ea0039985eb7de1fd0a69a9db9a03065767810
huangbaihua <huangbaihua001@gmail.com>
2015-07-01 15:32:36 +0800

    Update ModelRegister

--- ofbiz-smart-framework/src/test/java/org/huihoo/ofbiz/smart/test/ModelRegisterTest.java
+++ ofbiz-smart-framework/src/test/java/org/huihoo/ofbiz/smart/test/ModelRegisterTest.java
@@ -3,6 +3,7 @@ package org.huihoo.ofbiz.smart.test;
 import org.avaje.agentloader.AgentLoader;
 import org.huihoo.ofbiz.smart.entity.EntityConfiguration;
 import org.huihoo.ofbiz.smart.entity.ModelRegister;
+import org.huihoo.ofbiz.smart.test.model.Customer;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,5 +13,7 @@ public class ModelRegisterTest {
     AgentLoader.loadAgentFromClasspath("ofbiz-smart-agent","debug=1;packages=org.huihoo.ofbiz.smart.test.model.**");
     EntityConfiguration config = ModelRegister.me().getConfiguration();
     Assert.assertNotNull(config);
+    Customer customer = new Customer();
+    customer.save();
   }
 }
